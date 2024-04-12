907ea71731b07d9291dbe60e7e32009f5b59ca35
DanWeissman <dan.bayford@weissman.agency>
2024-04-12 14:52:02 +0100

    dockerised

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      // Required to run next-auth in Docker
+      allowedForwardedHosts: ['localhost:3000'],
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+};
 
 module.exports = nextConfig;
